9fa20313cda663533c57d670696f6101c70fa388
Angel Centeno <angelluiscenteno@gmail.com>
2016-04-25 11:36:08 -0400

    footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+<div id="footer" class="container-fluid">
+    <div class="container">
+          <section class="row">
+                <div class="col-md-12">
+                    <!-- FOOTER MENU -->
+                    <?php wp_nav_menu(array('menu'=>'Footer')); ?>
+                </div>
+          </section>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
