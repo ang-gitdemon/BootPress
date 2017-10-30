dda519a3faaf79814e5254820a2697181748165c
Angel Centeno <angel@angstudio3.com>
2017-10-30 18:33:33 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,25 +3,14 @@
 <div id="indexPage" class="container">
     <section class="row">
         <div id="post-content clearfix" class="col-md-8 innerp">
-          <?php if (have_posts()) : ?>
-          <?php while (have_posts()) : the_post(); ?>
+          
+            <?php
+            while ( have_posts() ) : the_post();
+              get_template_part( 'templates/content', 'single' );  
+            endwhile;
+            ?>
 
-                <h2><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <?php the_excerpt(); ?>
-                <div style="clear:both"></div>
-                  <a href="<?php echo the_permalink(); ?>">continue reading</a>
-                  <hr>
-
-              <?php endwhile; ?>
-              <?php else : ?>
-
-              <h2 class="center">Not Found</h2>
-              <p class="center">Sorry, but you are looking for something that isn't here.</p>
-              <?php get_search_form(); ?>
-              <?php endif; ?>
-              <?php wp_reset_query(); ?>
-
-          </div>
+        </div>
 
         <?php get_sidebar(); ?>
         
