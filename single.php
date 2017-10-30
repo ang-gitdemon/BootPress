5dc8c956c24ec8ada2570f7e542a89b19a057ed3
Angel Centeno <angel@angstudio3.com>
2017-10-30 18:31:12 -0400

    update single.php

--- single.php
+++ single.php
@@ -1,26 +1,18 @@
-<?php get_header(); ?>
+<? get_header(); ?>
 
-<div id="singlePage" class="container">
-	<section id="main-content" class="row">
-		<div class="col-md-8 innerp">
-				<?php if (have_posts()) : ?>
-                <?php while (have_posts()) : the_post(); ?>
+  <main class="container-fluid">
+    <div class="container">
+      <section class="row">
+        <?php
+        while ( have_posts() ) : the_post();
 
-            	<h2 class="singleTitlePage"><?php the_title(); ?></h2>
-				<div class="imgArea"><?php the_post_thumbnail('full'); ?></div>
-            	<p><?php the_content(); ?></p>
+          get_template_part( 'templates/content', 'single' );  
 
-				<?php endwhile; ?>
-                <?php else : ?>
-                <h2 class="center">Not Found</h2>
-                <p class="center">Sorry, but you are looking for something that isn't here.</p>
-                <?php get_search_form(); ?>
-                <?php endif; wp_reset_query();?>
-        </div>
+        endwhile;
+        ?>
+      </section>
+    </div>
+  </main>
 
-        	<?php get_sidebar(); ?>
+<? get_footer(); ?>
 
-	</section>
- </div>
-
-<?php get_footer(); ?>
