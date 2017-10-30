e02c1c6eb0e46184ce8f6fa0fb50c7a0900e9100
Angel Centeno <angel@angstudio3.com>
2017-10-30 18:33:49 -0400

    updated template part

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
           
             <?php
             while ( have_posts() ) : the_post();
-              get_template_part( 'templates/content', 'single' );  
+              get_template_part( 'templates/loop', 'single' );  
             endwhile;
             ?>
 
