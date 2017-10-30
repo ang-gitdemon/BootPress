c911051146b2e8f2b3ab4b050108cb2aba29f054
Angel Centeno <angel@angstudio3.com>
2017-10-30 18:37:39 -0400

    new footer
    js is now located in the functions.php which is called by get_footer

--- footer.php
+++ footer.php
@@ -1,16 +1,13 @@
 <div id="footer" class="container-fluid">
-    <div class="container">
-          <section class="row">
-                <div class="col-md-12">
-                    <!-- FOOTER MENU -->
-                    <?php wp_nav_menu(array('menu'=>'Footer')); ?>
-                </div>
-          </section>
-    </div>
+  <div class="container">
+    <section class="row">
+      <div class="col-lg-12">
+        <?php wp_nav_menu(array('menu'=>'Footer Menu' )); ?>
+      </div>
+    </section>
+  </div>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
 <?php wp_footer(); ?>
 
 </body>
