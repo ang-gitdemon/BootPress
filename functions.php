018c7a13ffb04484ff42aac9dcea4ce7e111628a
Angel Centeno <angel@angstudio3.com>
2017-10-30 18:29:34 -0400

    Enqueue Scripts and CSS
    Added new CDN for Bootstrap v4, Owl Carousel 2.2.1, and jQuery 3.2.1

--- functions.php
+++ functions.php
@@ -23,20 +23,30 @@ function custom_excerpt_length( $length ) {
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
-// Register our sidebars and widgetized areas. //
 
-function arphabet_widgets_init() {
-
-	register_sidebar( array(
-		'name'          => 'Home right sidebar',
-		'id'            => 'home_right_1',
-		'before_widget' => '<div>',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="rounded">',
-		'after_title'   => '</h2>',
-	) );
-
+// Enqueue Styles in Header
+function as3_css() {
+  wp_enqueue_style( 'propet-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'propet-style-min', get_stylesheet_directory_uri().'/css/style.min.css' );
+	wp_enqueue_style( 'bootstrap-min', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css' );
+	wp_enqueue_style( 'owl-theme-min', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css' );
+	wp_enqueue_style( 'owl-theme-min', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css' );
+  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+  	wp_enqueue_script( 'comment-reply' );
+  }
 }
-add_action( 'widgets_init', 'arphabet_widgets_init' );
+add_action( 'wp_enqueue_scripts', 'as3_css' );
+
+
+// Enqueue Scripts in Footer
+function as3_js() {
+	wp_enqueue_script( 'javascript', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+	wp_enqueue_script( 'popper-min', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js' );
+	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js');
+	wp_enqueue_script( 'owl-min', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js' );
+	wp_enqueue_script( 'main', get_stylesheet_directory_uri().'/js/main.js' );
+	wp_enqueue_script( 'font-awesome', 'https://use.fontawesome.com/e66dca7e8d.js' );
+}
+add_action( 'get_footer', 'as3_js' );
 
 ?>
