2a369eba242001b0266e755100dcfe4517cd0271
Angel Centeno <angel@angstudio3.com>
2018-04-14 19:46:38 -0400

    Update single.php

--- single.php
+++ single.php
@@ -1,18 +1,26 @@
-<? get_header(); ?>
+<?php get_header(); ?>
 
   <main class="container-fluid">
     <div class="container">
       <section class="row">
+        <?php if (have_posts()) : ?>
         <?php
         while ( have_posts() ) : the_post();
 
           get_template_part( 'templates/content', 'single' );  
 
         endwhile;
-        ?>
+        else : ?>
+
+        <h2 class="center">Not Found</h2>
+        <p class="center">Sorry, but you are looking for something that isn't here.</p>
+        <?php get_search_form(); ?>
+
+      <?php endif; ?>
+      <?php wp_reset_query(); ?>
       </section>
     </div>
   </main>
 
-<? get_footer(); ?>
+<?php get_footer(); ?>
 
