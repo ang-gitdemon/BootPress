b4edef7e152fa1fb70eeeeb92a8c697cf0bd15c8
Angel Centeno <angel@angstudio3.com>
2018-04-14 19:52:35 -0400

    Update and rename template-home to front-page.php

---
+++ front-page.php
@@ -0,0 +1,4 @@
+<?php /*Template Name: Home */ get_header(); ?>
+
+
+<?php get_footer(); ?>
--- template-home.php
+++
@@ -1,5 +0,0 @@
-<?php /* Template Name: Home */ get_header(); ?>
-
-<!-- HOME CODE GOES HERE -->
-
-<?php get_footer(); ?>
