c32f62a5d3c4e2ded8e693178ecc61e8cb22c58b
Angel Centeno <angel@angstudio3.com>
2018-04-14 19:45:13 -0400

    Update loop-content.php

---
+++ templates/loop-content.php
@@ -0,0 +1,8 @@
+
+
+  <h1><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h1>
+  <?php the_excerpt(); ?>
+  <div style="clear:both"></div>
+  <a href="<?php echo the_permalink(); ?>">continue reading</a>
+  <hr>
+
