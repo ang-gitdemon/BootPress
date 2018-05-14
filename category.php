b78e76e975ce758e315deffdebba90c51c8f3d7d
Angel Centeno <angel@angstudio3.com>
2018-05-14 17:49:02 -0400

    Update category.php

--- category.php
+++ category.php
@@ -1,51 +1,44 @@
 <?php get_header(); ?>
 
-<div id="catPage" class="container">
-	<section id="main-content" class="row">
+<div class="container">
+	<section class="row">
 		<div class="col-md-8 innerp">
 			<?php if (have_posts()) : ?>
-					<h2 class="single-title-cat"><?php printf( __( '%s' ), single_cat_title( '', false ) ); ?></h2>
-					<?php while (have_posts()) : the_post(); ?>
-
+				<h2 class="single-title-cat"><?php printf( __( '%s' ), single_cat_title( '', false ) ); ?></h2>
+			<?php while (have_posts()) : the_post(); ?>
 			<?php if ( has_post_thumbnail() ) { ?>
 				<div id="catRow" class="row">
 					<div class="col-md-3">
-						<div class="imgArea">
+						<div class="img-area">
 							<?php the_post_thumbnail('thumbnail'); ?>
 						</div>
 					</div>
 					<div class="col-md-9">
 						<h3><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h3>
 						<?php the_excerpt(); ?>
-                  		<div style="clear:both"></div>
-                  		<a href="<?php echo the_permalink(); ?>">continue reading</a>
+						<a href="<?php echo the_permalink(); ?>">continue reading</a>
 					</div>
 				</div>
-              	<hr>
-			<?php }
-			else { ?>
+			<?php } else { ?>
 				<div id="catRow" class="row">
 					<div class="col-md-12">
 						<h3><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h3>
 						<?php the_excerpt(); ?>
-                  		<div style="clear:both"></div>
-                  		<a href="<?php echo the_permalink(); ?>">continue reading</a>
+						<a href="<?php echo the_permalink(); ?>">continue reading</a>
 					</div>
 				</div>
-              	<hr>
 			<?php } ?>
+			<?php endwhile; ?>
+			<?php else : ?>
+				<h2 class="center">Not Found</h2>
+				<p class="center">Sorry, but you are looking for something that isn't here.</p>
+				<?php get_search_form(); ?>
+			<?php endif; wp_reset_query();?>
+		</div>
 
-				<?php endwhile; ?>
-                <?php else : ?>
-                <h2 class="center">Not Found</h2>
-                <p class="center">Sorry, but you are looking for something that isn't here.</p>
-                <?php get_search_form(); ?>
-                <?php endif; wp_reset_query();?>
-        </div>
-
-        	<?php get_sidebar(); ?>
+		<?php get_sidebar(); ?>
 
 	</section>
- </div>
+</div>
 
 <?php get_footer(); ?>
