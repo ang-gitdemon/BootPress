8bf92654c66f6aab150e5dccc185ec8d1f4085c7
Angel Centeno <angel.centeno@evokad.com>
2018-05-24 23:05:56 -0400

    Bootstrap update + js optimizing
    Updated to Bootstrap 4.1.0 and jQuery 3.3.1

--- functions.php
+++ functions.php
@@ -28,7 +28,7 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 function as3_css() {
   wp_enqueue_style( 'propet-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'propet-style-min', get_stylesheet_directory_uri().'/css/style.min.css' );
-	wp_enqueue_style( 'bootstrap-min', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css' );
+	wp_enqueue_style( 'bootstrap-min', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css' );
 	wp_enqueue_style( 'owl-theme-min', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css' );
 	wp_enqueue_style( 'owl-theme-min', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css' );
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
@@ -40,13 +40,20 @@ add_action( 'wp_enqueue_scripts', 'as3_css' );
 
 // Enqueue Scripts in Footer
 function as3_js() {
-	wp_enqueue_script( 'javascript', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
-	wp_enqueue_script( 'popper-min', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js' );
-	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js');
+	wp_enqueue_script( 'jquery-js', 'https://code.jquery.com/jquery-3.3.1.slim.min.js');
+	wp_enqueue_script( 'popper-min', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js' );
+	wp_enqueue_script( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js');
 	wp_enqueue_script( 'owl-min', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js' );
 	wp_enqueue_script( 'main', get_stylesheet_directory_uri().'/js/main.js' );
 	wp_enqueue_script( 'font-awesome', 'https://use.fontawesome.com/e66dca7e8d.js' );
 }
 add_action( 'get_footer', 'as3_js' );
+
+// Remove excess WP default items
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 ?>
